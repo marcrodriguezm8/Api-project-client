466d0e12253e25e24613782bee3e3712551beda4
marcrodriguezm8 <m.rodriguez.cefpnuria@gmail.com>
2024-05-05 20:51:23 +0200

    Created login and logout.

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Redirect;
 
 class LoginController extends Controller
 {
@@ -13,21 +15,24 @@ class LoginController extends Controller
         return view('login');
     }
     public function auth(Request $request){
-        $user = User::where('email', $request->email)->first();
-        if($user){
-            if(password_verify($request->password, $user->password)){
-                $response = Http::post('http://localhost:8000/api/login', [
-                    'email' => $user->email,
-                    'password' => $request->password,
-                ]);
-                if($response->successful()) {
-                    $response = $response->json();
-                    $token = $response['token'];
-                    session(['user_token' => $token]);
+        $credentials = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+        if(Auth::attempt($credentials)){
+            $response = Http::post('http://localhost:8000/api/login', [
+                'email' => $request->email,
+                'password' => $request->password,
+            ]);
+            if($response->successful()) {
+                $response = $response->json();
+                $token = $response['token'];
+                session(['user_token' => $token]);
 
-                    return redirect('/');
-                }
+                return Redirect::route('index');
             }
         }
+        return Redirect::route('login.index')->with('error', 'Email o contraseña incorrectos.');
+
     }
 }
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -10,13 +10,17 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
 
-        @vite(['resources/css/app.css'])
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans text-gray-900 antialiased">
+        <x-navbar></x-navbar>
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
             <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
                 <x-auth-session-status class="mb-4" :status="session('status')" />
-
+                @if (session('error'))
+                <p x-data="{ show: true }" x-show="show" x-transition x-init="setTimeout(() => show = false, 5000)"
+                    class="mb-5 text-red-600 text-sm dark:text-gray-400">{{ session('error') }}</p>
+                    @endif
                 <form method="POST" action="{{route('login.auth')}}">
                     @csrf
 
