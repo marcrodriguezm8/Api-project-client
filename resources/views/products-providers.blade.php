f19c057cca21d24bf3e0da9d23125732a144c8ce
marcrodriguezm8 <m.rodriguez.cefpnuria@gmail.com>
2024-05-07 20:58:15 +0200

    Update css.

--- resources/views/products-providers.blade.php
+++ resources/views/products-providers.blade.php
@@ -161,7 +161,7 @@
                         @elseif($key == 'provider_id')
                         <td data-popover-target="popover-default" scope="row" class="relative td px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                             <span class="product-id-td text-blue-700 underline">{{$value}}</span>
-                            <div data-popover id="popover-default" role="tooltip" class="absolute left-44 z-10 invisible inline-block w-64 text-sm text-gray-500 transition-opacity duration-300 bg-white border border-gray-200 rounded-lg shadow-sm opacity-0 dark:text-gray-400 dark:border-gray-600 dark:bg-gray-800">
+                            <div data-popover id="popover-default" role="tooltip" class="absolute left-44 z-10 invisible inline-block text-sm text-gray-500 transition-opacity duration-300 bg-white border border-gray-200 rounded-lg shadow-sm opacity-0 dark:text-gray-400 dark:border-gray-600 dark:bg-gray-800">
                                 <div class="px-3 py-2 bg-gray-100 border-b border-gray-200 rounded-t-lg dark:border-gray-600 dark:bg-gray-700">
                                     <h3 class="font-semibold text-gray-900 dark:text-white">{{$productProvider["provider_name"]}}</h3>
                                 </div>
