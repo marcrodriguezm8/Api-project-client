a26911ef776cf16413ddc8779df8985d034603b2
marcrodriguezm8 <m.rodriguez.cefpnuria@gmail.com>
2024-05-08 23:39:11 +0200

    Deleted dark mode on login and register.

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -14,16 +14,16 @@
     </head>
     <body class="font-sans text-gray-900 antialiased">
         <x-navbar></x-navbar>
-        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
-            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
+        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 ">
+            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white  shadow-md overflow-hidden sm:rounded-lg">
                 <x-auth-session-status class="mb-4" :status="session('status')" />
                 @if (session('success'))
                 <p x-data="{ show: true }" x-show="show" x-transition x-init="setTimeout(() => show = false, 5000)"
-                    class="mb-5 text-green-600 text-sm dark:text-gray-400">{{ session('success')[0] }}</p>
+                    class="mb-5 text-green-600 text-sm">{{ session('success')[0] }}</p>
                     @endif
                 @if (session('error'))
                 <p x-data="{ show: true }" x-show="show" x-transition x-init="setTimeout(() => show = false, 5000)"
-                    class="mb-5 text-red-600 text-sm dark:text-gray-400">{{ session('error')[0] }}</p>
+                    class="mb-5 text-red-600 text-sm ">{{ session('error')[0] }}</p>
                     @endif
                 <form method="POST" action="{{route('login.auth')}}">
                     @csrf
@@ -50,13 +50,13 @@
                     <!-- Remember Me -->
                     <div class="block mt-4">
                         <label for="remember_me" class="inline-flex items-center">
-                            <input id="remember_me" type="checkbox" class="rounded dark:bg-gray-900 border-gray-300 dark:border-gray-700 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800" name="remember">
-                            <span class="ms-2 text-sm text-gray-600 dark:text-gray-400">{{ __('Mantener sesión iniciada') }}</span>
+                            <input id="remember_me" type="checkbox" class="rounded  border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500 " name="remember">
+                            <span class="ms-2 text-sm text-gray-600 ">{{ __('Mantener sesión iniciada') }}</span>
                         </label>
                     </div>
                     <div class="mt-5">
-                        <span class="text-sm text-gray-600 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" >
-                                {{ __('Aún no estás registrado?') }} <a href="{{route('register.index')}}" class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800">{{ __('Regístrate ahora')}}</a>
+                        <span class="text-sm text-gray-600  rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 " >
+                                {{ __('Aún no estás registrado?') }} <a href="{{route('register.index')}}" class="underline text-sm text-gray-600  hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 ">{{ __('Regístrate ahora')}}</a>
                         </span>
                     </div>
                     <div class="flex items-center justify-end mt-4">
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -14,12 +14,12 @@
     </head>
     <body class="font-sans text-gray-900 antialiased">
         <x-navbar></x-navbar>
-        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
-            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
+        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 ">
+            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white  shadow-md overflow-hidden sm:rounded-lg">
                 <x-auth-session-status class="mb-4" :status="session('status')" />
                 @if (session('error'))
                 <p x-data="{ show: true }" x-show="show" x-transition x-init="setTimeout(() => show = false, 5000)"
-                    class="mb-5 text-red-600 text-sm dark:text-gray-400">{{ session('error') }}</p>
+                    class="mb-5 text-red-600 text-sm ">{{ session('error') }}</p>
                     @endif
                 <form method="POST" action="{{route('register.register')}}">
                     @csrf
@@ -60,8 +60,8 @@
                     </div>
 
                     <div class="mt-5">
-                        <span class="text-sm text-gray-600 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" >
-                                {{ __('Ya tienes una cuenta?') }} <a href="{{route('login.index')}}" class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800">{{ __('Inicia sesión')}}</a>
+                        <span class="text-sm text-gray-600  rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 " >
+                                {{ __('Ya tienes una cuenta?') }} <a href="{{route('login.index')}}" class="underline text-sm text-gray-600  hover:text-gray-900  rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 ">{{ __('Inicia sesión')}}</a>
                         </span>
                     </div>
                     <div class="flex items-center justify-end mt-4">
