1fe1f8a9fe27e30cfcac2f6a818b5fb5ce5f6612
marcrodriguezm8 <m.rodriguez.cefpnuria@gmail.com>
2024-05-08 23:43:31 +0200

    Deleted dark mode on components.

--- resources/views/components/modal.blade.php
+++ resources/views/components/modal.blade.php
@@ -2,7 +2,7 @@
 <div id={{$id}} tabindex="-1" aria-hidden="true" class="modal z-50 hidden overflow-y-auto overflow-x-hidden fixed flex justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
     <div class="relative p-4 w-full max-w-md max-h-full">
         <!-- Modal content -->
-        <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+        <div class="relative bg-white rounded-lg shadow ">
             <!-- Modal header -->
             {{$slot}}
         </div>
